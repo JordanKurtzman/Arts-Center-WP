9b8077405094375f10b3688a04b8760bb57bbf22
Jordan Kurtzman <jordankurtzman@gmail.com>
2021-07-27 16:34:47 -0700

    added footer links

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
             <span class="footer__text"><a class="footer__text--link" href="<?php echo home_url(); ?>"><span class="footer__text--icon"><i class="fas fa-home"></i></span> Ria Links for Artists Systems Changes</a></span>
             <div class="footer__links">
                 <span class="footer__text"> | </span>
-                <a href="#" class="footer__text"><i class="fab fa-twitter"></i></a>
-                <a href="#" class="footer__text"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="footer__text"><i class="fab fa-instagram"></i></a>
+                <a href="https://twitter.com/petrahalkes" class="footer__text"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.facebook.com/petra.halkes" class="footer__text"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/petra.halkes/" class="footer__text"><i class="fab fa-instagram"></i></a>
             </div>     
         </footer>
 <?php wp_footer() ?>            
